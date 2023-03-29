0ce206a1613b61223be3681fb4f64ebdb72595f4
Vera Zhong <verazhong@veras-mbp.myfiosgateway.com>
2023-03-29 12:15:30 -0400

    lol

---
+++ examples/how.js
@@ -0,0 +1,60 @@
+let x =0;
+let y=0;
+let img
+// let errorMessage;
+
+// function preload(){
+//     errorMessage=loadFont('/assets/fonts/Nunito-Regular.ttf',call(),err());
+// }
+function preload() {
+    img = loadImage('assets/img/ts.png')
+}
+
+
+function setup() {
+    createCanvas(400, 400);
+    //image(img, 0,0,400,400);
+  }
+  
+function draw() {
+    x = document.getElementById('x').value;
+    y = document.getElementById('y').value;
+    d = document.getElementById('d').value;
+    poseX = document.getElementById('selectPoseX').value;
+    operatorX = document.getElementById('selectOperationX').value;
+    poseY = document.getElementById('selectPoseY').value;
+    operatorY = document.getElementById('selectOperationY').value;
+    strokeColor = document.getElementById('stroke').value;
+    fillColor = document.getElementById('fill').value;
+    
+    poseXInt = parseInt(poseX);
+    xInt = parseInt(x);
+    yInt = parseInt(y);
+
+    image(img, 0,0,400,400);
+    textAlign(CENTER)
+    //console.log(x);
+    if(x>400 || y>400 || x<0 || y<0){
+        console.log('1')
+        rectMode(CENTER);
+        noStroke();
+        fill(255,0,0);
+        rect(width/2,height/2,width,50);
+        fill(0);
+        textFont('Helvetica');
+        text("You can't see the circle because it is being drawn outside of your canvas.",width/2,height/2)
+    }
+    
+    if(strokeColor !== '' && fillColor !== '') {
+      stroke(strokeColor);
+      fill(fillColor);
+    }
+    
+    
+    offsetX = Math.abs(eval(x+operatorX+poseX));
+    offsetY = Math.abs(eval(y+operatorY+poseY))
+
+    circle(offsetX, offsetY, d);
+
+  }
+
